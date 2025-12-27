dab8c44733a59311596527d2d5b46147867743c0
ZealDeal <pitt.dukes@gmail.com>
2025-12-27 12:20:42 +0530

    Add tests from Test Case Creation

---
+++ api-automation-framework/src/test/java/base/GetPetFindByTagsTest.java
@@ -0,0 +1,24 @@
+import base.TestBase;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+public class GetPetFindByTagsTest extends TestBase {
+
+    @Test
+    public void testGetPetFindByTags() {
+        String tags = "available,pending";
+
+        Response response = given()
+                .queryParam("tags", tags)
+                .when()
+                .get("/pet/findByTags")
+                .then()
+                .extract().response();
+
+        assertEquals(response.getStatusCode(), 200);
+        // assert response.json().get("id") != null;
+        // assert response.json().get("name") != null;
+    }
+}
