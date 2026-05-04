d3fddc4a1e3b644041f8a67ad2cef62ca6ecadc8
ZealDeal <pitt.dukes@gmail.com>
2026-05-04 10:46:40 +0530

    Add tests from Test Case Creation

---
+++ rest-assured-framework/src/test/java/clients/CsRepairClient_2.java
@@ -0,0 +1,32 @@
+package clients;
+
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class CsRepairClient extends BaseClient {
+
+    public Response postGraphql(String query) {
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("query", query);
+
+        return requestSpec()
+                .header("Content-Type", "application/json")
+                .body(payload)
+                .when()
+                .post("/api/v2/csrepair/data/rd/graphql");
+    }
+
+    public Response postGraphqlWithoutQuery() {
+        Map<String, Object> payload = new HashMap<>();
+
+        return requestSpec()
+                .header("Content-Type", "application/json")
+                .body(payload)
+                .when()
+                .post("/api/v2/csrepair/data/rd/graphql");
+    }
+}
